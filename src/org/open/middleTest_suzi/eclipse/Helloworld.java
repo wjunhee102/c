fd37a9b227c9287bf880eed3090865a62fab3744
junhee <wjunhee102@gmail.com>
2020-04-27 04:04:28 +0900

    자바 시작

---
+++ src/org/open/middleTest_suzi/eclipse/Helloworld.java
@@ -0,0 +1,14 @@
+package org.open.middleTest_suzi.eclipse;
+
+public class Helloworld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a;
+		a = 1;
+		long b = 2147483648L;
+		
+		System.out.println(b+a+"suzi said \"hello world!!\"\n HTML");
+	}
+
+}
